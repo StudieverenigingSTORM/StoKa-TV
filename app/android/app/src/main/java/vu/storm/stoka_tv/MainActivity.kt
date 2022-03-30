198609ab50a88e046e9eecf3b4182acc3b28898f
Ruben Horn <horn.ruben@yahoo.de>
2022-03-30 16:12:46 +0200

    Black splash screen

--- app/android/app/src/main/java/vu/storm/stoka_tv/MainActivity.kt
+++ app/android/app/src/main/java/vu/storm/stoka_tv/MainActivity.kt
@@ -2,7 +2,9 @@ package vu.storm.stoka_tv
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.FragmentActivity
 
 class MainActivity : FragmentActivity() {
@@ -13,6 +15,11 @@ class MainActivity : FragmentActivity() {
         setContentView(R.layout.activity_main)
         val webView = findViewById<WebView>(R.id.web_view)
         webView.settings.javaScriptEnabled = true
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                webView.visibility = View.VISIBLE;
+            }
+        }
         val url = getString(R.string.web_app_url)
         webView.loadUrl(url)
     }
