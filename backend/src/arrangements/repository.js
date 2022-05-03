29ca8e7a98714f0be925cc15d14df28bda8e8e1e
Ruben Horn <horn.ruben@yahoo.de>
2022-05-04 01:54:36 +0200

    some fixes

--- backend/src/arrangements/repository.js
+++ backend/src/arrangements/repository.js
@@ -24,7 +24,7 @@ class ArrangementsRepository {
     }
 
     getArrangement(arrangement) {
-        const arrangementDir = `${this.mediaDir}/${sanatizeFilenameInput(arrangement)}`
+        const arrangementDir = `${this.mediaDir}/${utils.sanatizeFilenameInput(arrangement)}`
         const arrangementFilename = `${arrangementDir}/arrangement.yaml`;
         const doesArrangementFileExist = fs.existsSync(arrangementFilename);
         let cfg = {};
@@ -59,7 +59,7 @@ class ArrangementsRepository {
     }
 
     getMediaFiles(arrangement) {
-        const arrangementDir = `${this.mediaDir}/${sanatizeFilenameInput(arrangement)}`
+        const arrangementDir = `${this.mediaDir}/${utils.sanatizeFilenameInput(arrangement)}`
         return fs.readdirSync(arrangementDir).filter(f => utils.hasValidExtension(f));
     }
 
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const baseUrl = window.location.href.split('/').slice(0, -1).join('/');
+
 define(function(require, exports, module) {
     module.exports = {
         title: 'StoKa-TV',
-        apiBaseUrl: '/api',
+        apiBaseUrl: baseUrl + '/api',
         titleTimeout: 3000,
         notFoundTimeout: 1500,
         reloadArrangementsInterval: 5000,
